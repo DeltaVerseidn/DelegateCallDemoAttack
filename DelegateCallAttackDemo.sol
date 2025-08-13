4b5b17fd435d64e1fd22dbab1a28a85bbd3358d5
Altz <faustaryo@gmail.com>
2025-08-13 07:12:06 +0700

    Update DelegateCallAttackDemo.sol

--- DelegateCallAttackDemo.sol
+++ DelegateCallAttackDemo.sol
@@ -2,6 +2,48 @@
 
 pragma solidity 0.8.20;
 
+contract Called{
+	uint public number;
+	event NumberChanged(address _from, uint newNumber);
+
+	function changeNumber(uint newNumber) public{
+		number = newNumber;
+		emit NumberChanged(msg.sender, newNumber);
+	}
+}
+
+contract Caller{
+	address public called;
+	uint public number;
+	address public owner;
+
+	constructor(address _called){
+		called = _called;
+		owner = msg.sender;
+	}
+
+	function delegateChangeNumber(uint newNumber) public{
+		(bool success, ) = called.delegatecall(
+			abi.encodeWithSignature("changeNumber(uint256)", newNumber)
+		);
+
+		require(success, "Delegate call failed");
+	}
+
+	function callChangeNumber(uint newNumber) public {
+		(bool success, ) = called.call(
+			abi.encodeWithSignature("changeNumber(uint256)", newNumber)
+		);
+
+		require(success, "Call Failed");
+	}
+
+	function changeCalledAddress(address newContract) public{
+		called = newContract;
+	}
+
+}
+
 interface TargetInterface {
     function delegateChangeNumber(uint newNumber) external;
 }
@@ -29,3 +71,4 @@ contract Attacker {
     }
 
 }
+
